2c520492518b24d3dee2bf6a99a9c827e8773aff
李锋 <lifeng@oudmon.com>
2017-05-10 18:06:04 +0800

    完善ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # ZRNotify
-deal with notification in chain
+
+	使用一种简洁的方式组织通知的代码逻辑
+
+## pod方式安装
+
+	`pod 'ZRNotify', '~> 0.0.5'`
+
+## 使用
+
+> 分开监测
+
+```swift
+lazy var zrNotify: ZRNotify = {
+    var zrnotify = ZRNotify()
+    zrnotify.on("ScheduleA", notify: { notify in
+        
+        print(notify.object ?? "nil object")
+    }).on("ScheduleB", notify: { notify in
+        
+        print(notify.object ?? "nil object")
+    }).on("ScheduleC", notify: { notify in
+        
+        print(notify.object ?? "nil object")
+    })
+    
+    return zrnotify
+ }()
+```
+
+> 统一处理监测结果
+
+```swift
+lazy var zrNotifys: ZRNotify = {
+    var zrnotify = ZRNotify()
+    zrnotify.ons(["Schedule1", "Schedule2", "Schedule3"], notify: { notify in
+        
+        print(notify.object ?? "nil object")
+    })
+    
+    return zrnotify
+}()
+```
+
+> 初始化通知接收
+
+```swift
+override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // 初始化
+    zrNotify.opStatusForAll(true)
+    zrNotifys.opStatusForAll(true)
+    
+    NotificationCenter.default.post(name: Notification.Name(rawValue: "ScheduleA"), object: "hello ScheduleA")
+    NotificationCenter.default.post(name: Notification.Name(rawValue: "ScheduleB"), object: nil)
+    NotificationCenter.default.post(name: Notification.Name(rawValue: "ScheduleC"), object: "hello ScheduleC")
+    
+    
+    NotificationCenter.default.post(name: Notification.Name(rawValue: "Schedule1"), object: "hello Schedule1")
+    NotificationCenter.default.post(name: Notification.Name(rawValue: "Schedule2"), object: nil)
+    NotificationCenter.default.post(name: Notification.Name(rawValue: "Schedule3"), object: "hello Schedule3")
+}
+```
--- ZRNotifyDemo/ZRNotifyDemo/ViewController.swift
+++ ZRNotifyDemo/ZRNotifyDemo/ViewController.swift
@@ -11,14 +11,33 @@ import ZRNotify
 
 class ViewController: UIViewController {
 
+    /*
+        1.
+     */
     lazy var zrNotify: ZRNotify = {
         var zrnotify = ZRNotify()
         zrnotify.on("ScheduleA", notify: { notify in
             
+            print(notify.object ?? "nil object")
         }).on("ScheduleB", notify: { notify in
             
+            print(notify.object ?? "nil object")
         }).on("ScheduleC", notify: { notify in
             
+            print(notify.object ?? "nil object")
+        })
+        
+        return zrnotify
+    }()
+    
+    /*
+        2.
+     */
+    lazy var zrNotifys: ZRNotify = {
+        var zrnotify = ZRNotify()
+        zrnotify.ons(["Schedule1", "Schedule2", "Schedule3"], notify: { notify in
+            
+            print(notify.object ?? "nil object")
         })
         
         return zrnotify
@@ -27,7 +46,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        zrNotify.opStatusForAll(true)
+        zrNotifys.opStatusForAll(true)
         
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "ScheduleA"), object: "hello ScheduleA")
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "ScheduleB"), object: nil)
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "ScheduleC"), object: "hello ScheduleC")
+        
+        
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "Schedule1"), object: "hello Schedule1")
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "Schedule2"), object: nil)
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "Schedule3"), object: "hello Schedule3")
     }
 
     override func didReceiveMemoryWarning() {
